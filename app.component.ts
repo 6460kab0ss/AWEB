c235a912feb0fe6cdc7ef0fae16a421f67f8fa3e
6460kab0ss <psubrecary@student.hau.edu.ph>
2025-01-20 11:35:01 +0800

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { interval } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { OnInit } from '@angular/core';
+import { MyserviceService } from './myservice.service';
+import { NewCmpComponent } from './new-cmp/new-cmp.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, NewCmpComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+
+todaydate;
+componentproperty;
+constructor(private myservice:MyserviceService) {
+  this.todaydate = this.myservice.showTodayDate();
+  this.componentproperty = this.myservice.serviceproperty;
+}
+
+  // title = 'ng-pipes-demo';
+  
+  // presentDate = new Date();
+  // // time$ =interval(1000).pipe(map(() =>new Date());
+  // price : number = 20000; ngOnInit () {}
+  
+  // Fruits = ["Apple", "Orange", "Grapes", "Mango", "Kiwi", "Pomegranate"];
+
+  // decimalNum1: number = 8.759;
+  // decimalNum2: number = 5.43;
+  
+  // testArray = [1, 2, 3, 4, 5, 6];
+
+  // testObject = {
+  //   name: 'LeBron James',
+  //   age: 25,
+  //   food: 'Tacos'
+  // };
+}
